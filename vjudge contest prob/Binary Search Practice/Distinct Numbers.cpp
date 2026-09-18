8c61a47eebb4b0b53447aa560c284b9d7d2e4d09
Ulfat <ulfat.vns@gmail.com>
2026-09-18 11:09:11 +0600

    Create Distinct Numbers.cpp

---
+++ vjudge contest prob/Binary Search Practice/Distinct Numbers.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sort(arr.begin(), arr.end());
+
+    int count = 1;
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] != arr[i-1])
+            count++;
+    }
+
+    cout << count;
+
+    return 0;
+}
